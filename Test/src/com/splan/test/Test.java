58d8fdaa7acfee77cec213efbd1b0983b7925726
Administrator <Administrator@192.168.1.9>
2019-05-06 11:14:53 +0530

    hello world commited

---
+++ Test/src/com/splan/test/Test.java
@@ -0,0 +1,13 @@
+package com.splan.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello Wolrd");
+		}
+
+	}
+
+}
